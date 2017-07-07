ef033784ca20b662367e72c7d0dd8e5f427108dc
chandrakanth <chandrakanththadakapally@gmail.com>
2017-07-07 18:36:38 -0500

    Creating Model Object and Initilizing Them

---
+++ ObjectInitilizer.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace OOPS_Cocepts{
+    class ObjectInitilizer{
+        public int Id;
+        public String Name;
+        public String Email;
+        public String Gender;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace OOPS_Cocepts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //If you have a Model in Class then you will Initilize Objects Like This
+            ObjectInitilizer OI1 = new ObjectInitilizer{ Id=700, Name="Chandrakanth", Email="chandu@gmail.com", Gender="Male"};
+            ObjectInitilizer OI2 = new ObjectInitilizer{ Id=641, Name="Thadkap", Email="Thadkapa@gmail.com", Gender="Male"};
+
+            Console.WriteLine(OI1.Id);
+            Console.WriteLine(OI1.Name);
+            Console.WriteLine(OI1.Email);
+            Console.WriteLine(OI1.Gender);
+            Console.WriteLine("\n\n");
+            Console.WriteLine(OI2.Id);
+            Console.WriteLine(OI2.Name);
+            Console.WriteLine(OI2.Email);
+            Console.WriteLine(OI2.Gender);
         }
     }
 }

d4912939bda0d1249eff9141043b43ddcaf14b11
chandrakanth <chandrakanththadakapally@gmail.com>
2017-07-09 20:08:22 -0500

    structure concept

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace OOPS_Cocepts
     {
         static void Main(string[] args)
         {
-           
+            Structure Structure1 = new Structure();
+           Structure1.method();
+           Structure1.Id = 10;
+           System.Console.WriteLine(Structure1.Id);
+           Structure Structure2 = Structure1;
+           Structure2.method();
+           Structure2.Id = 20;
+           System.Console.WriteLine(Structure1.Id);
+           System.Console.WriteLine(Structure2.Id);
+
         }
+
+        
     }
 }
---
+++ Structure.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OOPS_Cocepts{
+
+    struct Structure{
+        public int Id;
+        public void method(){
+            System.Console.WriteLine("It is a Structure");
+        }
+
+    }
+}
